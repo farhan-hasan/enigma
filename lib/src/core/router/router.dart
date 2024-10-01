e407c035e7946464bf019dc6dfaf2c08402d50d3
husky <nayemali14125513@gmail.com>
2024-10-01 12:43:09 +0600

    After merge changes

--- lib/src/core/router/router.dart
+++ lib/src/core/router/router.dart
@@ -1,6 +1,7 @@
 import 'package:bot_toast/bot_toast.dart';
-import 'package:enigma/src/features/auth/presentation/login/view/login_screen.dart';
-import 'package:enigma/src/features/auth/presentation/login/view/test_screen.dart';
+import 'package:enigma/src/features/auth/presentation/auth_screen/login/view/login_screen.dart';
+import 'package:enigma/src/features/auth/presentation/auth_screen/view/auth_screen.dart';
+import 'package:enigma/src/features/splash/presentation/view/splash_screen.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
@@ -13,9 +14,7 @@ final goRouterProvider = Provider(
         GoRoute(
           path: LoginScreen.route,
           builder: (context, state) {
-            return LoginScreen(
-              data: state.pathParameters,
-            );
+            return LoginScreen();
           },
         ),
         GoRoute(
@@ -34,14 +33,3 @@ final goRouterProvider = Provider(
     );
   },
 );
-final goRouterProvider = Provider((ref) {
-  return GoRouter(initialLocation: LoginScreen.route, observers: [
-    BotToastNavigatorObserver()
-  ], routes: [
-    GoRoute(
-        path: LoginScreen.route,
-        builder: (context, state) {
-          return LoginScreen();
-        }),
-  ]);
-});
