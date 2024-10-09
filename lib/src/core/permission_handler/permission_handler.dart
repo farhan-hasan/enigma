4111bf1f941c1b9035b4a4af8e5a5af81695a5b9
farhan-hasan <syedfarhan6491232@gmail.com>
2024-10-09 19:29:55 +0600

    Revert "garbage"
    This reverts commit 84b2f003c26937e81b148684caa1196adc992dee.

--- lib/src/core/permission_handler/permission_handler.dart
+++
@@ -1,9 +0,0 @@
-import 'package:permission_handler/permission_handler.dart';
-
-class PermissionHandler {
-  static Future<void> requestPermission() async {
-    PermissionStatus locationPermission = await Permission.location.request();
-    PermissionStatus bleScan = await Permission.bluetoothScan.request();
-    PermissionStatus bleConnect = await Permission.bluetoothConnect.request();
-  }
-}
